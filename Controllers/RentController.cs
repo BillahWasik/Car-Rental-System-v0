5b3b44ddd31b4ce3f7ec49ccf3aef31a9825e3ed
Wasik <Wasik@DESKTOP-P0PU1B1>
2022-11-09 11:59:49 +0600

    Controllers Added

---
+++ Controllers/RentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Rental_System.Controllers
+{
+    public class RentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
